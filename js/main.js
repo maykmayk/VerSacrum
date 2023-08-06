a46d7f0cf8db8ef3f6b6a10499234f5975b669cd
maykmayk <m.devita@ied.edu>
2023-08-06 21:50:29 +0200

    -

--- js/main.js
+++ js/main.js
@@ -18,38 +18,20 @@ _app.getDate = () => {
 _app.startUp = () => {
     _app.getDate();
 
-    _app.img1 = document.getElementById('img1');
-    const originalSrc = img1.src;
+    const images = document.querySelectorAll('[id^="img"]');
+    const originalSrcs = [];
 
-    img1.addEventListener("mouseover", () => {
-        img1.src = "./asset/images/fullSize/tee1Back.png";
-    });
+    for (let i = 0; i < images.length; i++) {
+        originalSrcs[i] = images[i].src;
 
-    img1.addEventListener("mouseout", () => {
-        img1.src = originalSrc; 
-    });
+        images[i].addEventListener("mouseover", () => {
+            images[i].src = `./asset/images/fullSize/tee${i + 1}Back.png`;
+        });
 
-    _app.img2 = document.getElementById('img2');
-    const originalSrc2 = img2.src;
-
-    img2.addEventListener("mouseover", () => {
-        img2.src = "./asset/images/fullSize/tee2Back.png";
-    });
-
-    img2.addEventListener("mouseout", () => {
-        img2.src = originalSrc2; 
-    });
-
-    _app.img3 = document.getElementById('img3');
-    const originalSrc3 = img3.src;
-
-    img3.addEventListener("mouseover", () => {
-        img3.src = "./asset/images/fullSize/tee3Back.png";
-    });
-
-    img3.addEventListener("mouseout", () => {
-        img3.src = originalSrc3; 
-    });
+        images[i].addEventListener("mouseout", () => {
+            images[i].src = originalSrcs[i];
+        });
+    }
 };
 
 _app.startUp();
