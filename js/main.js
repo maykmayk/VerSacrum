739e808df90b7015878b107c9dddb2ae6d2f0fdb
maykmayk <m.devita@ied.edu>
2023-08-06 12:37:25 +0200

    .

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const _app = {}
+
+_app.getDate = () => {
+    function getCurrentDateFormatted() {
+        const currentDate = new Date();
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const year = currentDate.getFullYear();
+        const hours = String(currentDate.getHours()).padStart(2, '0');
+        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
+    }
+    const formattedDate = getCurrentDateFormatted();
+    document.getElementById('date').innerText = formattedDate;
+}
+
+_app.startUp = () => {
+    _app.getDate()
+}
+
+_app.startUp();
