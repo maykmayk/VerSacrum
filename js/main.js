fd7f041d950489c84318b02b79e0622a73eeb0de
maykmayk <m.devita@ied.edu>
2023-08-23 11:47:42 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -15,71 +15,88 @@ _app.getDate = () => {
     document.getElementById('date').innerText = formattedDate;
 }
 
-_app.startUp = () => {
-    window.addEventListener('load', () => {
-        // La funzione viene chiamata quando tutti gli elementi della pagina sono stati caricati
+_app.loader = () => {
+	// La funzione viene chiamata quando tutti gli elementi della pagina sono stati caricati
       
         // Simuliamo il caricamento della pagina per 3 secondi
         setTimeout(() => {
-          // Nascondi il loader impostando l'opacità a 0
-          const loader = document.getElementById('loader');
-          loader.style.opacity = '0';
-      
-          // Nascondi il loader completamente dopo 1 secondo
-          setTimeout(() => {
-            loader.style.display = 'none';
-          }, 1000);
+			// Nascondi il loader impostando l'opacità a 0
+			const loader = document.getElementById('loader');
+			loader.style.opacity = '0';
+		
+			// Nascondi il loader completamente dopo 1 secondo
+			setTimeout(() => {
+			  loader.style.display = 'none';
+			}, 1000);
+  
+		  }, 3000);
+		
+		  // Aggiorna il numero di caricamento da 0 a 100 con curva di Bezier
+		  const loader = document.getElementById('loader');
+		  const numLoader = document.getElementById('numLoader');
+		  const duration = 1000; // Tempo in millisecondi per il caricamento completo
+		  let startTime = null;
+		
+		  function bezier(t) {
+			return t * t * (3 - 2 * t); // Funzione di curva di Bezier
+		  }
+		
+		  function updateLoader(timestamp) {
+			if (!startTime) {
+			  startTime = timestamp;
+			}
+		
+			const progress = Math.min(1, (timestamp - startTime) / duration); // Progresso normalizzato tra 0 e 1
+			const bezierValue = bezier(progress); // Calcola la curva di Bezier
+		
+			numLoader.textContent = Math.floor(bezierValue * 100); // Calcola il valore del numero con curva di Bezier
+		
+			if (progress < 1) {
+			  requestAnimationFrame(updateLoader);
+			} else {
+			  // Quando il caricamento raggiunge il 100%, nascondi il loader dopo 1 secondo impostando l'opacità a 0
+			  setTimeout(() => {
+				loader.style.opacity = '0';
+				setTimeout(() => {
+				  loader.style.display = 'none';
+				}, 1000);
+			  }, 1000);
+			}
+		  }
+		
+		  // Avvia l'aggiornamento del loader
+		  requestAnimationFrame(updateLoader);
+}
+_app.hoverProductTee = () => {
+	const images = document.querySelectorAll('[id^="img"]');
+    const originalSrcs = [];
 
-        }, 3000);
-      
-        // Aggiorna il numero di caricamento da 0 a 100 con curva di Bezier
-        const loader = document.getElementById('loader');
-        const numLoader = document.getElementById('numLoader');
-        const duration = 1000; // Tempo in millisecondi per il caricamento completo
-        let startTime = null;
-      
-        function bezier(t) {
-          return t * t * (3 - 2 * t); // Funzione di curva di Bezier
-        }
-      
-        function updateLoader(timestamp) {
-          if (!startTime) {
-            startTime = timestamp;
-          }
-      
-          const progress = Math.min(1, (timestamp - startTime) / duration); // Progresso normalizzato tra 0 e 1
-          const bezierValue = bezier(progress); // Calcola la curva di Bezier
-      
-          numLoader.textContent = Math.floor(bezierValue * 100); // Calcola il valore del numero con curva di Bezier
-      
-          if (progress < 1) {
-            requestAnimationFrame(updateLoader);
-          } else {
-            // Quando il caricamento raggiunge il 100%, nascondi il loader dopo 1 secondo impostando l'opacità a 0
-            setTimeout(() => {
-              loader.style.opacity = '0';
-              setTimeout(() => {
-                loader.style.display = 'none';
-              }, 1000);
-            }, 1000);
-          }
-        }
-      
-        // Avvia l'aggiornamento del loader
-        requestAnimationFrame(updateLoader);
-    });
+    for (let i = 0; i < images.length; i++) {
+        originalSrcs[i] = images[i].src;
 
-    const products = document.querySelectorAll('.product');
+        images[i].addEventListener("mouseover", () => {
+            images[i].src = `./asset/images/fullSize/tee${i + 1}Back.png`;
+        });
+
+        images[i].addEventListener("mouseout", () => {
+            images[i].src = originalSrcs[i];
+        });
+    }
+}
+_app.redirectPageTee = () => {
+	const products = document.querySelectorAll('.product');
     products.forEach(product => {
         product.addEventListener('click', () => {
           const name = product.dataset.name;
           const price = product.dataset.price;
+          const stories = product.dataset.stories;
           const image = product.dataset.image;
           const secondImage = product.dataset.secondImage;
     
           const productData = {
             name: name,
             price: price,
+			stories: stories,
             image: image,
             secondImage: secondImage
           };
@@ -87,34 +104,39 @@ _app.startUp = () => {
           sessionStorage.setItem('selectedProduct', JSON.stringify(productData));
         });
     });
-
-    // Recupera i dati del prodotto memorizzati nella sessionStorage
-    const productData = JSON.parse(sessionStorage.getItem('selectedProduct'));
-
-    // Inserisci i dati nelle rispettive posizioni
-    if (productData) {
-        document.getElementById('productImg').src = productData.image;
-        document.getElementById('name').innerText = productData.name;
-        document.getElementById('price').innerText = productData.price;
-        document.getElementById('secondProductImg').src = productData.secondImage;
-
-        document.getElementById('secondProductImg').addEventListener('click', () => {
-            // Scambia i percorsi delle immagini
-            const temp = productData.image;
-            productData.image = productData.secondImage;
-            productData.secondImage = temp;
-      
-            // Aggiorna le immagini nella pagina
-            document.getElementById('productImg').src = productData.image;
-            document.getElementById('secondProductImg').src = productData.secondImage;
-      
-            // Aggiorna i dati salvati nella sessionStorage
-            sessionStorage.setItem('selectedProduct', JSON.stringify(productData));
-          });
-    } else {
-    }
-
-    const container = document.getElementById("imgDetail");
+	const productData = JSON.parse(sessionStorage.getItem('selectedProduct'));
+	const productImages = [productData.image, productData.secondImage];
+	let currentImageIndex = 0;
+	const productImg = document.getElementById('productImg');
+  
+	function updateImage() {
+	  productImg.src = productImages[currentImageIndex];
+	}
+  
+	if (productData) {
+		updateImage();
+	
+		const sliderArrowLeft = document.getElementById('sliderArrowLeft');
+		const sliderArrowRight = document.getElementById('sliderArrowRight');
+	
+		sliderArrowLeft.addEventListener('click', () => {
+		  currentImageIndex = (currentImageIndex - 1 + productImages.length) % productImages.length;
+		  updateImage();
+		});
+	
+		sliderArrowRight.addEventListener('click', () => {
+		  currentImageIndex = (currentImageIndex + 1) % productImages.length;
+		  updateImage();
+		});	
+  
+	  document.getElementById('name').innerText = productData.name;
+	  document.getElementById('price').innerText = productData.price;
+	  document.getElementById('stories').innerText = productData.stories;
+	} else {
+	}
+}
+_app.productZoom = () => {
+	const container = document.getElementById("productImg");
     const img = document.getElementById("productImg");
     container.addEventListener("mousemove", onZoom);
     container.addEventListener("mouseover", onZoom);
@@ -129,25 +151,16 @@ _app.startUp = () => {
         img.style.transformOrigin = `center center`;
         img.style.transform = "scale(1)";
     }
+}
 
+_app.startUp = () => {
+    window.addEventListener('load', () => {
+		_app.loader();
+    });
+	_app.hoverProductTee();
+    _app.redirectPageTee();
+	_app.productZoom();
     _app.getDate();
-
-    const images = document.querySelectorAll('[id^="img"]');
-    const originalSrcs = [];
-
-    for (let i = 0; i < images.length; i++) {
-        originalSrcs[i] = images[i].src;
-
-        images[i].addEventListener("mouseover", () => {
-            images[i].src = `./asset/images/fullSize/tee${i + 1}Back.png`;
-        });
-
-        images[i].addEventListener("mouseout", () => {
-            images[i].src = originalSrcs[i];
-        });
-    }
 };
-
-
 
 _app.startUp();
